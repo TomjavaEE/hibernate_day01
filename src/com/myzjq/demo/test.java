f94a5d1a71e7ebd7187d14772b28bb75feea05d5
zjq <1660449279@qq.com>
2019-12-17 10:28:34 +0800

    第一次上传

--- src/com/myzjq/demo/test.java
+++ src/com/myzjq/demo/test.java
@@ -1,5 +1,18 @@
 package com.myzjq.demo;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class test {
+    public static void main(String[] args) {
+        Configuration configuration=new Configuration().configure();
+        SessionFactory sessionFactory=configuration.buildSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        System.out.println(currentSession);
+
+
+    }
 
 }
